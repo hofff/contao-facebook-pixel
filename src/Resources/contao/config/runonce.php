ab5cbcb1aaa3851c0fe9c4653fa77fd3a76ae262
David Molineus <david.molineus@netzmacht.de>
2018-11-07 16:53:05 +0100

    Make sure that field exists.

--- src/Resources/contao/config/runonce.php
+++ src/Resources/contao/config/runonce.php
@@ -7,11 +7,15 @@ namespace Hofff\Contao\FacebookPixel\RunOnce;
 use Contao\Database;
 
 (function (Database $database) {
-    $database->prepare('UPDATE tl_content %s WHERE type=?')
-        ->set(['type' => 'hofff_facebook_pixel_optout'])
-        ->execute('hofff_facebook-pixel-optout');
+    if ($database->fieldExists('type', 'tl_content')) {
+        $database->prepare('UPDATE tl_content %s WHERE type=?')
+            ->set(['type' => 'hofff_facebook_pixel_optout'])
+            ->execute('hofff_facebook-pixel-optout');
+    }
 
-    $database->prepare('UPDATE tl_module %s WHERE type=?')
-        ->set(['type' => 'hofff_facebook_pixel_optout'])
-        ->execute('hofff_facebook-pixel-optout');
+    if ($database->fieldExists('type', 'tl_module')) {
+        $database->prepare('UPDATE tl_module %s WHERE type=?')
+            ->set(['type' => 'hofff_facebook_pixel_optout'])
+            ->execute('hofff_facebook-pixel-optout');
+    }
 })(Database::getInstance());
