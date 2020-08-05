16e69fa66b6db90bf75b037a14ea41e896d83907
David Molineus <david.molineus@netzmacht.de>
2020-08-05 13:22:42 +0200

    Add support for consent bridge

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Hofff\Contao\Consent\Bridge\EventListener\Dca\ConsentIdOptions;
+
 $GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(
     '{publish_legend}',
-    '{facebook_pixel_legend},fb_pixel_id,fb_pixel_status;{publish_legend}',
+    '{facebook_pixel_legend},fb_pixel_id,fb_pixel_status,fb_pixel_consentId;{publish_legend}',
     $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
 );
 
@@ -20,4 +22,19 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['fb_pixel_status'] = [
     'inputType' => 'checkbox',
     'eval'      => ['tl_class' => 'w50 m12'],
     'sql'       => "char(1) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_page']['fields']['fb_pixel_consentId'] = [
+    'label'            => &$GLOBALS['TL_LANG']['tl_page']['fb_pixel_consentId'],
+    'exclude'          => true,
+    'inputType'        => 'select',
+    'options_callback' => [ConsentIdOptions::class, '__invoke'],
+    'eval'             => [
+        'tl_class'           => 'w50',
+        'includeBlankOption' => true,
+        'chosen'             => true,
+        'multiple'           => false,
+        'nullIfEmpty'        => true,
+    ],
+    'sql'              => ['type' => 'string', 'default' => null, 'notnull' => false],
 ];
--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -9,6 +9,7 @@ $GLOBALS['TL_LANG']['tl_page']['facebook_pixel_legend'] = $GLOBALS['TL_LANG']['t
 
 $GLOBALS['TL_LANG']['tl_page']['fb_pixel_id'] = array('Facebook Pixel ID', 'Bitte hier die Facebook Pixel ID eingeben.');
 $GLOBALS['TL_LANG']['tl_page']['fb_pixel_status'] = array('Aktiv?', 'Wenn aktiv, wird der Facebook Pixel auf allen Unterseiten eingebunden.');
+$GLOBALS['TL_LANG']['tl_page']['fb_pixel_consentId'] = array('Facebook Pixel Consent ID', 'Weisen Sie eine Consent ID zu.');
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['fb_pixel_opt_out_active_text'] = $GLOBALS['TL_LANG']['tl_page']['fb_pixel_opt_out_active_text'] =
     array('Text für OptOut Cookie setzen', 'Text für OptOut Cookie setzen');
